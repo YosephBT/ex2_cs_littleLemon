195b3c3040fb1ae086ab8db5f373bf5a2611cfe0
YosephBT <yosephbelay.t@gmail.com>
2025-06-10 01:31:21 +0300

    developed api endpoints

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import MenuSerializer
+from .models import Menu,Booking
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset=Menu.objects.all()
+    serializer_class=MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+    queryset=Menu.objects.all()
+    serializer_class=MenuSerializer
+
